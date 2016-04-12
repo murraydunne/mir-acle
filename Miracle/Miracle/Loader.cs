047fcd563ac3fdb596dd8fdf76b17713e3dd8c8b
M D <dudecrushman@gmail.com>
2016-04-11 20:33:21 -0700

    Improved the markov chain generator.

--- Miracle/Miracle/Loader.cs
+++ Miracle/Miracle/Loader.cs
@@ -9,24 +9,92 @@ namespace Miracle
 {
     public class Loader
     {
+        private class NoteOnOff
+        {
+            public int Data1Number { get; set; }
+            public int TicksOn { get; set; }
+        }
+
         public List<Note> LoadMidiFile(string name)
         {
-            Sequence file = new Sequence(name);
+            Sequence file = null;
+            try
+            {
+                file = new Sequence(name);
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+
             Track firstTrack = file[1];
+            List<NoteOnOff> onOffs = new List<NoteOnOff>();
 
-            List<Note> song = new List<Note>();
+            bool[] isOn = new bool[256];
+            int[] onAt = new int[256];
 
-            foreach(MidiEvent evt in firstTrack.Iterator())
+            IEnumerable<MidiEvent> events = firstTrack.Iterator();
+
+            foreach (MidiEvent evt in firstTrack.Iterator())
             {
                 if(evt.MidiMessage.MessageType == MessageType.Channel)
                 {
                     ChannelMessage msg = (ChannelMessage)evt.MidiMessage;
                     
-                    if(msg.Command == ChannelCommand.NoteOn)
+                    if(msg.Command == ChannelCommand.NoteOn && msg.Data2 > 0)
                     {
-                        song.Add(new Note(msg.Data1 - 45, NoteLength.Sixteenth));
+                        isOn[msg.Data1] = true;
+                        onAt[msg.Data1] = evt.AbsoluteTicks;
+                    }
+                    else if(msg.Command == ChannelCommand.NoteOff || (msg.Command == ChannelCommand.NoteOn && msg.Data2 == 0))
+                    {
+                        if(isOn[msg.Data1])
+                        {
+                            int onFor = evt.AbsoluteTicks - onAt[msg.Data1];
+                            onOffs.Add(new NoteOnOff() { Data1Number = msg.Data1, TicksOn = onFor });
+                            isOn[msg.Data1] = false;
+                        }
                     }
                 }
+            }
+
+            int sixteenthNoteTicks = 10000;
+            foreach(NoteOnOff onOff in onOffs)
+            {
+                if(onOff.TicksOn < sixteenthNoteTicks)
+                {
+                    sixteenthNoteTicks = onOff.TicksOn;
+                }
+            }
+
+            List<Note> song = new List<Note>();
+
+            foreach (NoteOnOff onOff in onOffs)
+            {
+                int noteId = onOff.Data1Number - 45;
+                NoteLength length = NoteLength.Sixteenth;
+
+                if(onOff.TicksOn >= sixteenthNoteTicks * 2)
+                {
+                    length = NoteLength.Eighth;
+                }
+
+                if (onOff.TicksOn >= sixteenthNoteTicks * 4)
+                {
+                    length = NoteLength.Quarter;
+                }
+
+                if (onOff.TicksOn >= sixteenthNoteTicks * 8)
+                {
+                    length = NoteLength.Half;
+                }
+
+                if (onOff.TicksOn >= sixteenthNoteTicks * 16)
+                {
+                    length = NoteLength.Whole;
+                }
+
+                song.Add(new Note(noteId, length));
             }
 
             return song;
